78097a173a8e9d7858c8a653784e21fb3007ebbc
Thiago Bocchile <tykoth@gmail.com>
2007-12-05 23:19:40 +0000

    Products

---
+++ app/controllers/product_groups_controller.php
@@ -0,0 +1,16 @@
+<?php
+class ProductGroupsController extends AppController {
+   var $name = 'ProductGroups';
+   var $uses = array();
+   
+   function system_index()
+   {
+   	
+   }
+   
+   function system_add()
+   {
+   	
+   }
+}
+?>
